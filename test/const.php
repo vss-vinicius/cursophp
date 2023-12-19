b44d8704bdaf6f166c376f7cb5773f20b9bf85e9
Vinicius Soares <100010777+vss-vinicius@users.noreply.github.com>
2023-12-19 19:24:53 -0300

    Create const.php

---
+++ test/const.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo constante em PHP</title>
+</head>
+
+<body>
+    <?php
+    const CANAL = "Curso em Vídeo \u{1F499} ";
+    echo "Eu adoro o " . CANAL;
+    echo "Estamos no ano de " . date('Y');
+    ?>
+</body>
+
+</html>
