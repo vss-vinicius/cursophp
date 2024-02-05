1a8f97d1c5816bef1b3612a07663cc51a254efc0
Vinicius Soares <100010777+vss-vinicius@users.noreply.github.com>
2024-02-05 17:52:22 -0300

    Update resposta.php

--- challenges/d001/resposta.php
+++ challenges/d001/resposta.php
@@ -3,7 +3,9 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Desafio PHP</title>
 </head>
 
@@ -17,11 +19,13 @@
             $a = $n - 1;
             $s = $n + 1;
 
-            echo "O número escolhido foi $n";
-            echo "<br>O seu antecessor é $a";
-            echo "<br>O seu sucessor é $s";
+            echo "O número escolhido foi <strong>$n</strong>";
+            echo "<br>O seu <em>antecessor</em> é $a";
+            echo "<br>O seu <em>sucessor</em> é $s";
             ?>
         </p>
+
+        <button onclick="javascript:history.go(-1)">&#x2B05; Voltar</button>
     </main>
 </body>
 
